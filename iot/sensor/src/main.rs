76a9c9b040f0a5c8eeb02b6c87ab5726d0a971f9
Arthur Lamidel <arthurlamidel@gmail.com>
2026-02-13 11:55:40 +0100

    Update main.rs

--- iot/sensor/src/main.rs
+++ iot/sensor/src/main.rs
@@ -54,6 +54,7 @@ async fn send_loop(client: AsyncClient, unique_id: u8) {
         }
         log::trace!("Data sent: {data:?}");
 
-        // Wait before sending new data time::sleep(Duration::from_secs(24_u64.saturing_add_signed(random_range((-4)..=4)));
+        // Wait before sending new data
+ time::sleep(Duration::from_secs(24_u64.saturing_add_signed(random_range((-4)..=4)))).await;
     }
 }
