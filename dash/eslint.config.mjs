256f8839ce027cd2355d588fbe9648094d8593d2
OlivierFacelina <olivier.facelina@gmail.com>
2026-02-13 09:17:00 +0100

    Update ESLint configuration to allow default project for .mjs files

--- dash/eslint.config.mjs
+++ dash/eslint.config.mjs
@@ -10,6 +10,7 @@ export default defineConfig(
   {
     languageOptions: {
       parserOptions: {
+        allowDefaultProject: ['*.mjs', 'dist/*.js'],
         projectService: true,
       },
     },
