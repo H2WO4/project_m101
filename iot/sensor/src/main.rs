96b3b844f57ba77ed31113ccec6685e0406d92f0
H2WO4 <arthurlamidel@gmail.com>
2026-02-13 09:15:00 +0100

    Add documentation

--- iot/sensor/src/main.rs
+++ iot/sensor/src/main.rs
@@ -20,34 +20,41 @@ async fn main() {
         .parse()
         .expect("ID should be a number");
 
+    // Configure and launch the MQTT client
     let mqtt_options = {
         let mut mqtt_options = MqttOptions::new(format!("sensor-{unique_id}"), "mqtt", 1883);
         mqtt_options.set_keep_alive(Duration::from_secs(120));
         mqtt_options
     };
-
     let (client, mut event_poll) = AsyncClient::new(mqtt_options, 10);
 
+    // Start a loop; sending new data to the MQTT broker
+    task::spawn(async move { send_loop(client, unique_id).await });
+
+    // The client's event loop must be polled in order to progress
+    // Even if we are not subscribed to any topic
+    while event_poll.poll().await.is_ok() {}
+}
+
+/// Report the sensor's information to the MQTT broker
+async fn send_loop(client: AsyncClient, unique_id: u8) {
+    let wait_time = 24;
     let topic = format!("traffic/{unique_id}");
 
-    task::spawn(async move {
-        let wait_time = 24;
+    loop {
+        let avg_speed: u32 = ((unique_id as u32) * 17 % 43) + 10;
+        let data = vec![avg_speed as u8];
 
-        loop {
-            let avg_speed: u32 = ((unique_id as u32) * 17 % 43) + 10;
-            let data = vec![avg_speed as u8];
-
-            let result = client
-                .publish(topic.clone(), QoS::AtLeastOnce, true, data.clone())
-                .await;
-            if let Err(error) = result {
-                log::error!("Error: {error}");
-            }
-            log::trace!("Data sent! {data:?}");
-
-            time::sleep(Duration::from_secs(wait_time as u64)).await;
+        // While a performance loss, data must be cloned to be able to be logged afterwards
+        let result = client
+            .publish(&topic, QoS::AtLeastOnce, true, data.clone())
+            .await;
+        if let Err(error) = result {
+            log::error!("Error: {error}");
         }
-    });
+        log::trace!("Data sent: {data:?}");
 
-    while event_poll.poll().await.is_ok() {}
+        // Wait before sending new data
+        time::sleep(Duration::from_secs(wait_time as u64)).await;
+    }
 }
